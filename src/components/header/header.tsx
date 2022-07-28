5a3b52e55af636f2723588b0dab6df730fcadac2
A <alexandre_caruso@dell.com>
2022-07-28 15:01:16 -0300

    content: first home page customization

---
+++ src/components/header/header.tsx
@@ -0,0 +1,17 @@
+import * as React from "react"
+import PropTypes from "prop-types"
+import {textCenter} from "./header.module.css"
+
+const Header = ({ siteTitle }) => (
+  <header className={textCenter}>{siteTitle}</header>
+)
+
+Header.propTypes = {
+  siteTitle: PropTypes.string,
+}
+
+Header.defaultProps = {
+  siteTitle: ``,
+}
+
+export default Header
