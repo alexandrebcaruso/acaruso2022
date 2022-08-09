7b9aa27ff9fa7f3bcc3b9359f4d4561ce5eab73b
A <alexandre_caruso@dell.com>
2022-08-09 16:06:28 -0300

    feat: work on logo

--- src/components/logo/logo.tsx
+++ src/components/logo/logo.tsx
@@ -3,9 +3,8 @@ import { logo } from "./logo.module.css";
 
 const Logo = () => (
     <a className={logo} href="https://www.linkedin.com/in/alexandreborsattocaruso/" target="_blank">
-        <svg>
-            <line x1="0" x2="50%" y1="100%" y2="0"></line>
-            <line x1="100%" x2="50%" y1="100%" y2="0"></line>
+        <svg xmlns="http://www.w3.org/2000/svg" width="80" height="130">
+            <path fill="none" stroke="#AA0000" stroke-width="2" d="M 0,129.44 L 40,0 L 80,129.44" />
         </svg>
     </a>
 )
